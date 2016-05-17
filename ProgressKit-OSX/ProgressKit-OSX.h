eea8b742fdf5ec8db04fff834f493757f37a5961
Sergio Gutierrez <sergio@gokarumi.com>
2016-05-18 00:07:50 +0200

    Oops, now it's a OSX framework

---
+++ ProgressKit-OSX/ProgressKit-OSX.h
@@ -0,0 +1,19 @@
+//
+//  ProgressKit-OSX.h
+//  ProgressKit-OSX
+//
+//  Created by Sergio on 18/05/16.
+//  Copyright © 2016 Kauntey Suryawanshi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ProgressKit-OSX.
+FOUNDATION_EXPORT double ProgressKit_OSXVersionNumber;
+
+//! Project version string for ProgressKit-OSX.
+FOUNDATION_EXPORT const unsigned char ProgressKit_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressKit_OSX/PublicHeader.h>
+
+
