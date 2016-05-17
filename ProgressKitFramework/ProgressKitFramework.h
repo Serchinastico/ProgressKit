a4bc027a92355246288621372f2e3d24ee59de93
Sergio Gutierrez <sergio@gokarumi.com>
2016-05-18 00:06:06 +0200

    Create framework target

---
+++ ProgressKitFramework/ProgressKitFramework.h
@@ -0,0 +1,19 @@
+//
+//  ProgressKitFramework.h
+//  ProgressKitFramework
+//
+//  Created by Sergio on 18/05/16.
+//  Copyright © 2016 Kauntey Suryawanshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProgressKitFramework.
+FOUNDATION_EXPORT double ProgressKitFrameworkVersionNumber;
+
+//! Project version string for ProgressKitFramework.
+FOUNDATION_EXPORT const unsigned char ProgressKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressKitFramework/PublicHeader.h>
+
+
