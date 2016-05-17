dcf424f44138a4b124c3d72d6f11d522950118d4
Sergio Gutierrez <sergio@gokarumi.com>
2016-05-18 00:13:06 +0200

    Another attempt

---
+++ ProgressKitOSX/ProgressKitOSX.h
@@ -0,0 +1,19 @@
+//
+//  ProgressKitOSX.h
+//  ProgressKitOSX
+//
+//  Created by Sergio on 18/05/16.
+//  Copyright © 2016 Kauntey Suryawanshi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ProgressKitOSX.
+FOUNDATION_EXPORT double ProgressKitOSXVersionNumber;
+
+//! Project version string for ProgressKitOSX.
+FOUNDATION_EXPORT const unsigned char ProgressKitOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressKitOSX/PublicHeader.h>
+
+
